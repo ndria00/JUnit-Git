63841e6840a3e44762bd712b4cce103fbc911ec6
ndria00 <ctrndr00e01i872o@studenti.unical.it>
2021-10-28 08:03:34 +0200

    first commit

---
+++ src/main/java/it/unical/demacs/JUnit_Git/App.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.JUnit_Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
